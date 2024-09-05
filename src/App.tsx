900b5724fec99ec63eface803da4c780d832ac7f
Artiu <oborskiartur123@gmail.com>
2024-09-05 22:21:27 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { createSignal } from "solid-js";
+
+function App() {
+	const [count, setCount] = createSignal(0);
+
+	return (
+		<>
+			<div class="text-9xl">Test</div>
+		</>
+	);
+}
+
+export default App;
